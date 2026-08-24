24d367f29a8413e6656add3ceeee947dabe27e47
nocerinofortunato <nocerinofortunato@gmail.com>
2026-08-24 18:20:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,85 @@
-const CACHE="agenda-personale-v2";
-const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",event=>event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request))));
+const CACHE="agenda-personale-v3";
+
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>
+        Promise.all(
+          keys
+            .filter(k=>k!==CACHE)
+            .map(k=>caches.delete(k))
+        )
+      )
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+
+  event.respondWith(
+
+    fetch(event.request)
+      .then(response=>{
+
+        const copy=response.clone();
+
+        caches.open(CACHE)
+          .then(cache=>{
+            cache.put(event.request,copy);
+          });
+
+        return response;
+
+      })
+      .catch(()=>{
+
+        return caches.match(event.request);
+
+      })
+
+  );
+
+});
+
 self.addEventListener("notificationclick",event=>{
+
   event.notification.close();
-  event.waitUntil(clients.matchAll({type:"window",includeUncontrolled:true}).then(list=>{
-    if(list.length)return list[0].focus();
-    return clients.openWindow("./");
-  }));
+
+  event.waitUntil(
+
+    clients.matchAll({
+      type:"window",
+      includeUncontrolled:true
+    })
+
+    .then(list=>{
+
+      if(list.length){
+        return list[0].focus();
+      }
+
+      return clients.openWindow("./");
+
+    })
+
+  );
+
 });
